d47f05843576bf85949f9c3e027633e9c4dab2e2
Ramon Vieira <ramonvieira122333@gmail.com>
2023-08-30 03:08:39 -0300

    Ajeitando problema de diretório nas imagens

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,49 +1,49 @@
 export const cards = [
     {
         id: "1",
-        back: "./public/images/1.png",
+        back: "./images/1.png",
         title: "Chef",
     },
 
     {
         id: "2",
-        back: "./public/images/2.png",
+        back: "./images/2.png",
         title: "Doctor",
     },
 
     {
         id: "3",
-        back: "./public/images/3.png",
+        back: "./images/3.png",
         title: "Architect",
     },
 
     {
         id: "4",
-        back: "./public/images/4.png",
+        back: "./images/4.png",
         title: "Judge",
     },
 
     {
         id: "5",
-        back: "./public/images/5.png",
+        back: "./images/5.png",
         title: "Programmer",
     },
 
     {
         id: "6",
-        back: "./public/images/6.png",
+        back: "./images/6.png",
         title: "Civil Engineer",
     },
 
     {
         id: "7",
-        back: "./public/images/7.png",
+        back: "./images/7.png",
         title: "Scientist",
     },
 
     {
         id: "8",
-        back: "./public/images/8.png",
+        back: "./images/8.png",
         title: "Astronaut",
     },
 ]
